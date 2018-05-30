c0bd3dfcd06b3f861525456cbfd0e10264f02b7c
vasskats <vasskats@yahoo.com>
2018-05-31 02:02:22 +0300

    Day 11

--- tsepas/server.js
+++ tsepas/server.js
@@ -5,11 +5,15 @@ var app = express();
 var mysql = require('mysql');
 
 app.use(express.static('./'));
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
 
 app.post('/books/', function(req, res){
-	res.send('books');
-	
+	res.send(req.body);
+	res.send(req.headers);
 });
+
 
 
 app.listen(8081, () => console.log('Listening on port 8081'));
--- tsepas/test.js
+++ tsepas/test.js
@@ -19,34 +19,32 @@ var httpRequest;
 
 document.getElementById('ajaxCall').addEventListener('click', makeRequest);
 
-//document.getElementById('ajaxCall').onclick = function() {
-//};
-
 function makeRequest() {
-
+	/* Creating a new book to insert to the db */
 	var tAuthor = document.getElementById('author').value;
 	var tTitle = document.getElementById('title').value;
 	var tGenre = document.getElementById('genre').value;
 	var tPrice = document.getElementById('price').value;
 	
 	var b = new Book(tAuthor, tTitle, tGenre, tPrice);
-	console.log(JSON.stringify(b));
+	console.log(b);
 
 	httpRequest = new XMLHttpRequest();
 
 	if(!httpRequest) {
 		alert('Failed');
 	}
+	var a = 2;
 
 	httpRequest.onreadystatechange = alertContents;
-	httpRequest.open('GET', 'index.html', true);
-	httpRequest.send();
+	httpRequest.open('POST', '/books/');
+	httpRequest.setRequestHeader('Content-Type', 'x-www-form-urlencoded');
+	httpRequest.send('Just text'+a);
 
 	function alertContents() {
-		if (httpRequest.readyState === XMLHttpRequest.Done) {
+		if (httpRequest.readyState === XMLHttpRequest.DONE) {
 			if (httpRequest.status === 200) {
-				var response = httpRequest.responseText;
-				alert(response);
+				console.log(httpRequest.response);
 				} else {
 					alert('There was a problem');
 				}
