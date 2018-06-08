7bb172b136880f9f065d3938daf4263dc1b73265
dvorf <vasskats@yahoo.com>
2018-06-08 20:41:30 +0300

    The end (HopefullY)

--- tsepas/server.js
+++ tsepas/server.js
@@ -16,16 +16,23 @@ app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(function(req, res, next) { setTimeout(next, 2000)});
 // DB connection
 var connection = mysql.createConnection({
 	host     : 'localhost',
-	user     : 'root',
-	password : '04215mom',
+	user     : 'it21633',
+	password : '',
 	database : 'lib'
 });
 
-app.get('/books/:id', function(req, res) {
-	res.send(req.params);
+connection.connect();
+
+app.get('/books/', function(req, res) {
+	var title = '%' + req.query.title + '%'; // To a match strings containing the keyword
+	connection.query('select * from books where title like ?', [title], function(error, results, fields) {
+		if (error) throw error;
+		res.send(JSON.stringify(results, null, ' ' ));
+	});
 });
 
 app.post('/books', function(req, res){
@@ -34,27 +41,22 @@ app.post('/books', function(req, res){
 	var genre = req.body.genre;
 	var price = req.body.price;
 
-	connection.connect();
 
 	connection.on('error', function(err) {
 		console.log(err.code); // 'ER_BAD_DB_ERROR'
+		res.send("Didn't commit to the database");
 	});
-
-
-//connection.query('select max(id) from books', function (error, results, fields) {
-//
-//	console.log(results[0]);
-//	console.log(results.insertId);
-//
-//});
-
-
 
 	connection.query('INSERT INTO books VALUES(?, ?, ?, ?, ?)', [0, author, title, genre, price], function (error, results, fields) {
 		if (error) throw error;
 		console.log('The solution is: ', results[0]);
+		res.format({
+			'text/html': function() {
+				res.send('<h1 align=center>All Good</h1>');
+			}
+		});
 	});
 });
 
-app.listen(8081, () => console.log('Listening on port 8081'));
+app.listen(3000, () => console.log('Listening on port 3000..'));
 
