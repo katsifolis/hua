3bb0cf9c26cbbd0a0115a60a56e57fb485961964
vasskats <vasskats@yahoo.com>
2018-11-28 22:16:16 +0000

    chad

---
+++ chad/it21633_ex_1_1.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+
+# Lists the contents of the home folder which are over 100M in descending order
+
+find ~/ -maxdepth 1 -type f -size +1k -exec ls -lh {} + | sort -hr -k 5
---
+++ chad/it21633_ex_1_2.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/bash
+
+# Lists files within a dir with zero file size and their empty subdirs
+
+dir=$1
+
+if [ -d "$dir" ]; then
+	flnms=`ls "$dir"` # all names into a list 		
+	cd $dir
+	echo "-- Empty Files --"
+
+	# Separating the regular files from directories
+	for i in $flnms; 
+	do
+		if [ -f "$i" ]; 
+		then 
+			flnm=`cat "$i"`
+			if [ "$flnm" == "" ];
+			then
+				echo "$i"
+			fi
+		fi
+	done
+	echo -e "\n-- Empty Directories --"
+
+	# Printing the empty folders
+	find -type d -empty -exec basename {} \; | sed 's/regexp/\n/g'
+else
+	echo Given file is not a directory!
+	exit
+fi
