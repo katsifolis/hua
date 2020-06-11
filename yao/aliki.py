dc4d0307ea59f3befe038ecb9fbc7e3eb2c38902
oxilgk <vazkats@gmail.com>
2020-06-11 19:55:03 +0300

    commit 'cause of fear of losing progress

--- yao/aliki.py
+++ yao/aliki.py
@@ -18,16 +18,6 @@ assoc = {} # Dictionary of labels and their corresponding value
 keys  = [] # Array of Alice's keys
 
 
-def ret_val(res):
-    for gid, v in res.items():
-        for value in v:
-            try:
-               # pprint("gid: " + str(gid) + " - " + str(assoc[value]))
-                return assoc[value]
-            except:
-               # print('err')
-               pass
-
 
 def byte_xor(ba1, ba2):
     return bytes([_a ^ _b for _a, _b in zip(ba1, ba2)])
@@ -127,7 +117,7 @@ def binary_gate(x, y, gid, gtype):
     # 2. the input values encrypted  #
     return c, keys 
 
-def full_adder():
+def full_adder(x, y, cin = 0):
     """
 
      Full adder Implementation
@@ -151,15 +141,11 @@ def full_adder():
         # 1 1 1  | 1  1 #
         # # # # # # # # # 
         """
-    get_bin = lambda x: format(x, 'b')
 
     # Inputs
-    x = 0   
-    y = 1  
-    cin = 0
 
     seq = ['XOR', 'XOR', 'AND', 'AND', 'OR']
-    r_values = [0,1,2]
+    r_values = [-1, -1, -1, -1, -1]
 
     
     global assoc
@@ -170,86 +156,83 @@ def full_adder():
     garbled_table, key_table[0] = binary_gate(x, y, 0, seq[0])
     garbled_tables[0] = garbled_table
     res = bob(garbled_tables, key_table)
-    r_values[0] = ret_val(res)
+    r_values[4] = ret_val(res)
     del(garbled_tables[0])
 
 
     # 2nd XOR
-    garbled_table, key_table[1] = binary_gate(r_values[0], cin, 1, seq[1])
+    garbled_table, key_table[1] = binary_gate(r_values[4], cin, 1, seq[1])
     garbled_tables[1] = garbled_table
     res = bob(garbled_tables, key_table)
     r_values[0] = ret_val(res)
-    print(r_values[0])
     del(garbled_tables[1])
 
     # 1st AND
-    garbled_table, key_table[2] = binary_gate(r_values[0], cin, 2, seq[2])
+    garbled_table, key_table[2] = binary_gate(r_values[4], cin, 2, seq[2])
     garbled_tables[2] = garbled_table
     res = bob(garbled_tables, key_table)
-    val = ret_val(res)
+    r_values[1] = ret_val(res)
     del(garbled_tables[2])
 
     # 2nd AND
     garbled_table, key_table[3] = binary_gate(x, y, 3, seq[3])
     garbled_tables[3] = garbled_table
     res = bob(garbled_tables, key_table)
-    r_values[1] = ret_val(res)
+    r_values[2] = ret_val(res)
     del(garbled_tables[3])
 
     # OR
-    garbled_table, key_table[4] = binary_gate(r_values[0], r_values[1], 4, seq[4])
+    garbled_table, key_table[4] = binary_gate(r_values[1], r_values[2], 4, seq[4])
     garbled_tables[4] = garbled_table
     res = bob(garbled_tables, key_table)
-    r_values[2] = ret_val(res)
-    print(r_values[2])
+    r_values[3] = ret_val(res)
     del(garbled_tables[4])
+
+    print("Cout: " + str(r_values[3]))
+    print("S:    " + str(r_values[0]))
+
+    return (r_values[3], r_values[0])
     
 
 
-    # Finding the key
-    
-    #garbled_tables, key_table = {}, {}
-
-    #for i in range(2, 5):
-    #    garbled_table, key_table[i] = binary_gate(x, y, i, seq[i])
-
-    #    garbled_tables[i] = garbled_table
-
-    #res = bob(garbled_tables, key_table)
-    ## Finding the key
-    #for gid, v in res.items():
-    #    for value in v:
-    #        try:
-    #            pprint("gid: " + str(gid) + " - " + str(assoc[value]))
-    #            break
-    #        except:
-    #            print('err')
-
-
-
-def aliki():
-
-    global assoc
-    key_table = {}
-    garbled_tables = {}             # Permuted garbled tables to be sent to bob
-
-    x      = 0
-    y      = 1
-    g_id   = 1
-    g_type = 'AND'
-
-    garbled_table, key_table[g_id] = binary_gate(x, y, g_id, g_type)
-    garbled_tables[g_id] = garbled_table
-
-    res = bob(garbled_tables, key_table)
-    # Finding the key
+def ret_val(res):
     for gid, v in res.items():
         for value in v:
             try:
-                pprint("gid: " + str(gid) + " - " + str(assoc[value]))
-                break
+               # pprint("gid: " + str(gid) + " - " + str(assoc[value]))
+                return assoc[value]
             except:
-                print('err')
+               # print('err')
+               pass
+
+def aliki():
+
+    # The Input
+    a   = "11111"  
+    b   = "10110"  
+    Cin = 0      # First iteration Cin = 0
+
+    # Array of 1-bit full adder output
+    res      = ['0'] * (len(a) + 1)
+
+    for i in range(len(a)-1, -1, -1):
+        x, y = int(a[i], 2), int(b[i], 2)
+        print(x)
+        print(y)
+
+        Cout, S = full_adder(x, y, Cin)
+        Cin = Cout
+        res[i+1]  = str(S)
+        if i == 0:
+            res[i] = str(Cout)
+            break
+
+    sum = 0 
+    res.reverse()
+    for i, v in enumerate(res):
+        sum += pow(2, i) * int(v)
+
+    print(sum)
 
 def bob(table, inputs):
     cipher_dict = {}
@@ -266,5 +249,4 @@ def bob(table, inputs):
 
     return cipher_dict
 
-
-full_adder()
+aliki()
